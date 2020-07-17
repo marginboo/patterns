22f78a377c2e44544b1b37bf058079f780c0f12b
marginboo <g18m6734@HUG77>
2020-07-17 20:49:33 +0200

    first day

--- Patterns/Library.fs
+++ Patterns/Library.fs
@@ -1,8 +1,20 @@
 ﻿#if !INTERACTIVE
 module Patterns
 #endif
+type Cell =
+| Black of char  
+| White of char
+| Unknown of char
 
-let toCells v = failwith "Not implemented"
+let toCells v = 
+    let b = Seq.toList v
+    let rec vells d lst = 
+        match d with 
+        |[] -> lst
+        |a::rest -> 2 //change to list of cells
+    vells b 2 // calls the recursive function
+
+failwith "Not implemented"
 
 let fromCells v = failwith "Not implemented" 
 
