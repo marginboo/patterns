52a07308f263efe4ebdba57ffed69b5c820f649b
marginboo <g18m6734@HUG77>
2020-07-21 20:45:59 +0200

    mw

--- Patterns/Library.fs
+++ Patterns/Library.fs
@@ -2,21 +2,46 @@
 module Patterns
 #endif
 type Cell =
-| Black of char  
-| White of char
-| Unknown of char
+| Black   
+| White 
+| Unknown 
+
+type Pattern = 
+| BlackP 
+| WhiteP
+| UnknownP
+| ZeroOrMore of Pattern
+| OneOrMore of  Pattern
+| Exactly of (int * Pattern)
+| FewerThan of (int * Pattern)
+| Sequence of Pattern list  
+| Either of (Pattern * Pattern)
+| Anything 
+| EndOfCells 
 
 let toCells v = 
-    let b = Seq.toList v
-    let rec vells d lst = 
+    let b = Seq.toList v  
+    let rec vells d (lst: Cell list) = 
         match d with 
         |[] -> lst
-        |a::rest -> 2 //change to list of cells
-    vells b 2 // calls the recursive function
+        |a::rest ->  vells rest ( lst @ [match a with 
+                                         | 'b' | 'B' -> Black
+                                         | 'w' | 'W' -> White
+                                         | _  -> Unknown]) //change to list of cells
+    vells b  [] // calls the recursive function
 
 failwith "Not implemented"
 
-let fromCells v = failwith "Not implemented" 
+let fromCells v =
+    let rec fells b lst = 
+        match b with 
+        |[] -> lst
+        |a::rest -> fells rest (lst + string (match a with 
+                                              | Black  -> 'b'
+                                              | White -> 'w'
+                                              | Unknown -> '.' ))
+    fells v ""
+failwith "Not implemented" 
 
 let patternMatch pattern cells = failwith "Not implemented"
 
